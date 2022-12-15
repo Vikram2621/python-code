58de0b949251986f5ac1b060b583bf112493b4fb
Vikram2621 <120091803+Vikram2621@users.noreply.github.com>
2022-12-15 16:00:37 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,20 @@
+class student:
+    name=""
+    age=0
+
+s=student()
+s.name="Name"
+s.age="Age"
+
+
+s1=student()
+s1.name="aanis"
+s1.age=19
+
+s2=student()
+s2.name="swift"
+s2.age=20
+
+print(s.name,s.age)
+print(s1.name,s1.age)
+print(s2.name,s1.age)
